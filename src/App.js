2b74a61d9536757ea2d957fb3ac91138018a4976
Aldi Dwi Ferdian <102952947+aldidwiferdian@users.noreply.github.com>
2023-06-13 10:56:14 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Basic from "./components/Basic"
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <h1>Aldi Dwi Ferdian 19.01.53.0097</h1>
+      <LandingPage />
       <Basic />
     </div>
   );
