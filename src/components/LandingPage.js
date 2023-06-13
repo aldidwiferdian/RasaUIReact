d5afc98c73653c088d7e1f22487547ab9d729c43
Aldi Dwi Ferdian <102952947+aldidwiferdian@users.noreply.github.com>
2023-06-13 13:56:05 +0700

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -14,6 +14,7 @@ function LandingPage() {
       {showPopup && (
         <div className="popupContainer">
           <div className="popupContent">
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Signum-Node-Blue.svg/512px-Signum-Node-Blue.svg.png?20211205034017" alt="Logo" className="logo" />
             <h2>Selamat Datang di Informasi Chatbot Wisata Tangeran Selatan!</h2>
             <p>Anda bisa memulainya dengan menekan tombol mulai dibawah ini.</p>
             <button className="button" onClick={closePopup}>Mulai</button>
